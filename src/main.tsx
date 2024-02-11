1c3607ef108217a1686ba28799699b5555d5fdf7
Jamie Livingstone <jamie.livingstone@outlook.com>
2024-02-11 10:17:16 +0000

    feat: tauri component library configuration

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
-import './styles.css';
+import { MantineProvider } from '@mantine/core';
+import '@mantine/core/styles.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <MantineProvider defaultColorScheme="dark">
+      <h1>Hello world!</h1>
+    </MantineProvider>
   </React.StrictMode>,
 );
